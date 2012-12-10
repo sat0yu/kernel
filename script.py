1b23ea5688cd55581831f0d3d576a23590eda8ea
yusuke sato <yusuke1.sato9.git@gmail.com>
2012-12-10 10:56:22 +0900

    ノード対に対して、線形カーネル以外にも適用できるように改良

--- script.py
+++ script.py
@@ -5,14 +5,29 @@ from kernel import *
 from graph import *
 
 if __name__=='__main__':
-    V = numpy.array([[1,-1,1,-1,1],[1,-1,1,0,1],[1,-1,1,1,0],[1,1,1,-1,-1]])
+    V = numpy.array([
+            [ 1, -1, 1, -1,  1],
+            [ 1, -1, 1,  0,  1],
+            [ 1, -1, 1,  1,  0],
+            [ 1,  1, 1, -1, -1]
+            ])
 
     # element must be following style, (label, (sn, en))
-    E = [(1,(0,1)),(1,(0,2)),(1,(0,3)),(1,(1,2)), (-1, (2,3))]
+    E = [
+        ( 1, (0,1)),
+        ( 1, (0,2)),
+        ( 1, (0,3)),
+        ( 1, (1,2)),
+        (-1, (2,3)),
+        ]
     
     g = Graph(V, E)
     y = g.E['label']
 
+    def gauss(x1, x2):
+        return numpy.exp( -1.0 * 0.1 * (numpy.linalg.norm(x1 - x2)** 2) )
+
+    #gm = g.grammatrix(gauss)
     gm = g.grammatrix()
     gkv = g.kernelfuncvector()
 
