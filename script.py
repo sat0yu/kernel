85a9d11e47b7f188a383a473e313bb6f45b78cea
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2012-12-12 06:56:08 +0900

    ノード集合Vをテキストデータから読み込めるようにした

--- script.py
+++ script.py
@@ -5,12 +5,16 @@ from kernel import *
 from graph import *
 
 if __name__=='__main__':
-    V = numpy.array([
-            [ 1, -1, 1, -1,  1],
-            [ 1, -1, 1,  0,  1],
-            [ 1, -1, 1,  1,  0],
-            [ 1,  1, 1, -1, -1]
-            ])
+
+#     V = numpy.array([
+#             [ 1, -1, 1, -1,  1],
+#             [ 1, -1, 1,  0,  1],
+#             [ 1, -1, 1,  1,  0],
+#             [ 1,  1, 1, -1, -1]
+#             ])
+
+    #numpy.savetxt('V.txt', V)
+    V = numpy.loadtxt('V.txt', dtype=numpy.float)
 
     # element must be following style, (label, (sn, en))
     E = [
@@ -20,23 +24,23 @@ if __name__=='__main__':
         ( 1, (1,2)),
         (-1, (2,3)),
         ]
+    numpy.savetxt('E.txt', E)
     
     g = Graph(V, E)
-    y = g.E['label']
-
     def gauss(x1, x2):
-        return numpy.exp( -1.0 * 0.1 * (numpy.linalg.norm(x1 - x2)** 2) )
+        return numpy.exp( -1.0 * 0.5 * (numpy.linalg.norm(x1 - x2)** 2) )
 
-    #gm = g.grammatrix(gauss)
-    gm = g.grammatrix()
-    gkv = g.kernelfuncvector()
+    gm = g.grammatrix(gauss)
+    func = gm.regression(g.E['label'], 0.1)
 
-    print 'gram: \n', gm
-    print 'label: ', y
-
-    func = gm.regression(numpy.matrix(y).T, 1.0)
-
-    kv = gkv( g.edge( (1,3) ) )
-    print kv
-    print func( numpy.matrix(kv).T )
+    kv = g.kernelfuncvector( g.edge( (0,1) ), gauss)
+    print func( kv )
+    kv = g.kernelfuncvector( g.edge( (0,2) ), gauss)
+    print func( kv )
+    kv = g.kernelfuncvector( g.edge( (0,3) ), gauss)
+    print func( kv )
+    kv = g.kernelfuncvector( g.edge( (1,2) ), gauss)
+    print func( kv )
+    kv = g.kernelfuncvector( g.edge( (2,3) ), gauss)
+    print func( kv )
 
