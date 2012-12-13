3763c7810749594be5b529835b738ec7209ebdbf
Yusuke Sato <yusuke1.sato9.git@gmail.com>
2012-12-13 11:21:00 +0900

    線形カーネルとガウスカーネルの比較:成績（2教科）を模したデータで、近い分布を持つ人にリンクを張った

--- script.py
+++ script.py
@@ -10,7 +10,7 @@ if __name__=='__main__':
     E = numpy.loadtxt('E.txt', dtype=numpy.int, comments='#')
     g = Graph(V, E)
 
-    beta = 0.5
+    beta = 0.005
     def graphkernel(x, y):
         s = numpy.exp( -beta * (numpy.linalg.norm(x[0] - y[0])** 2))
         e = numpy.exp( -beta * (numpy.linalg.norm(x[1] - y[1])** 2))
@@ -28,30 +28,13 @@ if __name__=='__main__':
     gm_2 = GramMatrix(g.E['data'], kernel=graphkernel_2)
     func_2 = gm_2.regression(g.E['label'], 0.01)
 
-    print '\nregression edge(8, [78~82])'
-    print func( g.edge( (8,78) ) ), func_2( g.edge( (8,78) ) )
-    print func( g.edge( (8,79) ) ), func_2( g.edge( (8,79) ) )
-    print func( g.edge( (8,80) ) ), func_2( g.edge( (8,80) ) )
-    print func( g.edge( (8,81) ) ), func_2( g.edge( (8,81) ) )
-    print func( g.edge( (8,82) ) ), func_2( g.edge( (8,82) ) )
-
-    print '\nregression edge(9, [78~82])'
-    print func( g.edge( (9,78) ) ), func_2( g.edge( (9,78) ) )
-    print func( g.edge( (9,79) ) ), func_2( g.edge( (9,79) ) )
-    print func( g.edge( (9,80) ) ), func_2( g.edge( (9,80) ) )
-    print func( g.edge( (9,81) ) ), func_2( g.edge( (9,81) ) )
-    print func( g.edge( (9,82) ) ), func_2( g.edge( (9,82) ) )
-
-    print '\nregression edge(8, [97~101])'
-    print func( g.edge( (8,97) ) ), func_2( g.edge( (8,97) ) )
-    print func( g.edge( (8,98) ) ), func_2( g.edge( (8,98) ) )
-    print func( g.edge( (8,99) ) ), func_2( g.edge( (8,99) ) )
-    print func( g.edge( (8,100) ) ), func_2( g.edge( (8,100) ) )
-    print func( g.edge( (8,101) ) ), func_2( g.edge( (8,101) ) )
-
-    print '\nregression edge(9, [97~101])'
-    print func( g.edge( (9,97) ) ), func_2( g.edge( (9,97) ) )
-    print func( g.edge( (9,98) ) ), func_2( g.edge( (9,98) ) )
-    print func( g.edge( (9,99) ) ), func_2( g.edge( (9,99) ) )
-    print func( g.edge( (9,100) ) ), func_2( g.edge( (9,100) ) )
-    print func( g.edge( (9,101) ) ), func_2( g.edge( (9,101) ) )
+    print '\nregression edge(4, [0~8])'
+    print func( g.edge( (4,0) ) ), func_2( g.edge( (4,0) ) )
+    print func( g.edge( (4,1) ) ), func_2( g.edge( (4,1) ) )
+    print func( g.edge( (4,2) ) ), func_2( g.edge( (4,2) ) )
+    print func( g.edge( (4,3) ) ), func_2( g.edge( (4,3) ) )
+    print func( g.edge( (4,4) ) ), func_2( g.edge( (4,4) ) )
+    print func( g.edge( (4,5) ) ), func_2( g.edge( (4,5) ) )
+    print func( g.edge( (4,6) ) ), func_2( g.edge( (4,6) ) )
+    print func( g.edge( (4,7) ) ), func_2( g.edge( (4,7) ) )
+    print func( g.edge( (4,8) ) ), func_2( g.edge( (4,8) ) )
